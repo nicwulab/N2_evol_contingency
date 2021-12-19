57f9a5a2cafa0021597273c1778d963b0440df24
Nicholas Wu <wchnicholas@Nicholass-MacBook-Pro.local>
2021-12-18 22:41:32 -0600

    add analysis for SD93 mut lib

--- README.md
+++ README.md
@@ -11,9 +11,33 @@
 1. Merge overlapping paired-end reads   
 ```pear -f [FASTQ FILE FOR FORWARD READ] -r [FASTQ FILE FOR FORWARD READ] -o [OUTPUT FASTQ FILE]```
     - Output files should be placed in the fastq/ folder and named as:
-      - SD93_ipt.fastq for input library
-      - SD93_rep1.fastq for post-passaged library (replicate 1)
-      - SD93_rep2.fastq for post-passaged library (replicate 2)
+      - SD93_ipt.assembled.fastq for input library
+      - SD93_rep1.assembled.fastq for post-passaged library (replicate 1)
+      - SD93_rep2.assembled.fastq for post-passaged library (replicate 2)
+
+2. Count mutants from fastq files   
+```python3 script/SD93_NA_fastq2count.py```
+    - Input files:
+      - [./Fasta/SD93_mutlib_ref.fa](./Fasta/SD93_mutlib_ref.fa)
+      - fastq/SD93_ipt.assembled.fastq
+      - fastq/SD93_rep1.assembled.fastq
+      - fastq/SD93_rep2.assembled.fastq
+    - Output file:
+      - [./result/SD93_MultiMutLib.tsv](./result/SD93_MultiMutLib.tsv)
+
+3. Filtering low count variants   
+```python3 script/SD93_NA_filter.py```
+    - Input file:
+      - [./result/SD93_MultiMutLib.tsv](./result/SD93_MultiMutLib.tsv)
+    - Output file:
+      - [./result/SD93_MultiMutLib_filtered.tsv](./result/SD93_MultiMutLib_filtered.tsv)
+
+4. Plot post-passaged frequency of each variant   
+```Rscript script/SD93_NA_plot_freq.R```
+   - Input file:
+     - [./result/SD93_MultiMutLib_filtered.tsv](./result/SD93_MultiMutLib_filtered.tsv)
+   - Output file:
+     - [./graph/SD93_mutlib_rep_compare.png](./graph/SD93_mutlib_rep_compare.png)
 
 ### Identifying permissive mutations for N336H
 1. Merge overlapping paired-end reads
---
+++ script/SD93_NA_filter.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import os
+import sys
+import numpy as np
+from Bio import SeqIO
+from collections import Counter
+
+def convert_ID_seq(mutlist, muts):
+  seq = ''
+  for mut_ref in muts:
+    pos = mut_ref[1:-1]
+    presence = 0
+    for mut in mutlist.rsplit('-'):
+      if pos in mut:
+        seq+=mut[-1]
+        presence = 1
+        break
+    if presence == 0:
+      seq+=mut_ref[0]
+  return (seq)
+
+def formatting(infile,outfile,cutoff,muts):
+  print ("writing: %s" % outfile)
+  infile  = open(infile,'r')
+  outfile = open(outfile,'w')
+  for line in infile.readlines(): 
+    if 'mut' in line:
+      outfile.write(line.rstrip()+"\t"+"denovo"+"\n")
+    else:
+      info = line.rstrip().rsplit("\t")
+      mutlist  = info[0]
+      mutclass = int(info[1])
+      iptcount = int(info[2])
+      R1count  = int(info[3])
+      R2count  = int(info[4])
+      R1freq   = float(info[5])
+      R2freq   = float(info[6])
+      denovo = []
+      if max([iptcount, R1count, R2count]) < cutoff:
+        continue 
+      for mut in mutlist.rsplit('-'):
+        if mut not in muts:
+          denovo.append(mut)
+      denovo = 'no' if len(denovo) == 0 else '-'.join(denovo)
+      outfile.write(line.rstrip()+"\t"+denovo+"\n")
+      if R1freq > 0.001 and R2freq > 0.001:
+        seq = convert_ID_seq(mutlist, muts)
+        print (seq)
+  infile.close()
+  outfile.close()
+
+def main():
+  cutoff = 10
+  muts    = ['E248G', 'R249K', 'I265T', 'Y336H', 'R338L', 'N339D', 'K344E', 'S346G', 'E369K', 'G381E', 'N387K']
+  infile  = 'result/SD93_MultiMutLib.tsv'
+  outfile = 'result/SD93_MultiMutLib_filtered.tsv'
+  formatting(infile, outfile, cutoff, muts)
+
+if __name__ == "__main__":
+  main()
